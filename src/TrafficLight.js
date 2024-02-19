11dc07821fac35215d95ed9d99fbe4aaec1bb547
ignaciofrag <ignaciofrag@gmail.com>
2024-02-20 00:53:53 +0100

    Arreglo final

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -2,27 +2,27 @@ import React, { useState } from 'react';
 import './TrafficLight.css';
 
 const TrafficLight = () => {
-  const [light, setLight] = useState(null);
+    const [color, setColor] = useState("red"); 
 
-  return (
-    <div id="trafficTop">
-      <div className="poste"></div> {}
-      <div id="container">
-        <div
-          className={`light red ${light === 'red' ? 'selected' : ''}`}
-          onClick={() => setLight('red')}
-        ></div>
-        <div
-          className={`light yellow ${light === 'yellow' ? 'selected' : ''}`}
-          onClick={() => setLight('yellow')}
-        ></div>
-        <div
-          className={`light green ${light === 'green' ? 'selected' : ''}`}
-          onClick={() => setLight('green')}
-        ></div>
-      </div>
-    </div>
-  );
+    return (
+        <div id="trafficTop">
+            <div className="poste"></div>
+            <div id="container">
+                <div
+                    className={`light red ${color === 'red' ? 'selected' : ''}`}
+                    onClick={() => setColor('red')}
+                ></div>
+                <div
+                    className={`light yellow ${color === 'yellow' ? 'selected' : ''}`}
+                    onClick={() => setColor('yellow')}
+                ></div>
+                <div
+                    className={`light green ${color === 'green' ? 'selected' : ''}`}
+                    onClick={() => setColor('green')}
+                ></div>
+            </div>
+        </div>
+    );
 };
 
 export default TrafficLight;
